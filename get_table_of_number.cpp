bf8c8bcf5767dc2fec285f272489b82607784cb6
dev jayswal <rdssjayswal@gmail.com>
2022-11-28 18:09:19 +0530

    this is simple program to get table of number enter by user

---
+++ get_table_of_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+ using namespace std;
+  int main(){
+    int n;
+    cout<<"plese enter a number "<<endl;
+    cin>>n;
+    for(int i=0;i<=20;i++){
+        cout<<n<<"*"<<i<<"="<<n*i<<endl;
+
+    }
+    return 0;
+  }
